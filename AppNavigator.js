d86215bb4dae22bf555e098cd5110ca27e75b879
novakvova <novakvova@gmail.com>
2019-04-21 12:43:28 +0300

    Begin create LoginScreen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import LoginPage from './app/components/auth/login/LoginPage';
 
 class HomeScreen extends React.Component {
   render() {
@@ -10,6 +11,10 @@ class HomeScreen extends React.Component {
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Login"
+          onPress={() => this.props.navigation.navigate('Login')}
         />
         <Button
           title="Go to Details... again"
@@ -46,7 +51,8 @@ class DetailsScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home:  HomeScreen,
-    Details: DetailsScreen
+    Details: DetailsScreen,
+    Login: LoginPage
   },
   {
     initialRouteName: "Home"
