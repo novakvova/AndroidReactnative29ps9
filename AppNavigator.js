814d2caaacb2af312495a7b570ed895572b7e995
novakvova <novakvova@gmail.com>
2019-05-05 11:18:26 +0300

    Add Image by RegisterPage

--- AppNavigator.js
+++ AppNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import LoginPage from './app/components/auth/login/LoginPage';
+import RegisterPage from "./app/components/auth/register/RegisterPage";
 
 class HomeScreen extends React.Component {
   render() {
@@ -15,6 +16,10 @@ class HomeScreen extends React.Component {
         <Button
           title="Login"
           onPress={() => this.props.navigation.navigate('Login')}
+        />
+        <Button
+          title="Реєстрація"
+          onPress={() => this.props.navigation.navigate('Register')}
         />
         <Button
           title="Go to Details... again"
@@ -52,7 +57,8 @@ const AppNavigator = createStackNavigator(
   {
     Home:  HomeScreen,
     Details: DetailsScreen,
-    Login: LoginPage
+    Login: LoginPage,
+    Register: RegisterPage
   },
   {
     initialRouteName: "Home"
---
+++ app/components/auth/register/RegisterPage.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    TouchableHighlight,
+    Image,
+    Alert
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+import axios from 'axios';
+
+
+export default class RegisterPage extends Component {
+
+    constructor(props) {
+        super(props);
+        state = {
+            email: '',
+            password: '',
+            photo: null
+        }
+    }
+
+    handleChoosePhoto = () => {
+        const options = {
+            noData: true,
+        };
+        ImagePicker.launchImageLibrary(options, response => {
+            //console.log('-----Choose image-----', response);
+              if (response.uri) {
+                this.setState({ photo: response });
+              }
+        });
+    };
+
+    onClickListener = (viewId) => {
+        Alert.alert("Alert", "Button pressed " + viewId);
+    }
+    onSubmit = () => {
+
+        console.log('----submit form-----', this.state);
+        // DO NOT DO THIS IF SHARING PRIVATE DATA WITH SERVICE
+        //const httsAgent = new https.Agent({ rejectUnauthorized: false });
+        // axios.get('http://10.7.180.167/api/values',{}, httsAgent)
+        //     .then( 
+        //         (data) => { console.log('--get data--', data);},
+        //         (error) => { console.log('--Bad request--');}
+        //     );
+        const model = { email: this.state.email, password: this.state.password };
+        axios.post('http://10.0.2.2:65127/api/account/login', model)
+            .then(
+                (data) => { console.log('--get data--', data); },
+                (error) => { console.log('--Bad request--'); }
+            );
+        // axios.get('https://10.7.180.167/api/values',{}, httsAgent)
+        //     .then( 
+        //         (data) => { console.log('--get data--', data);},
+        //         (error) => { console.log('--Bad request--');}
+        //     );
+    }
+
+    render() {
+        let photo=null; 
+        if(this.state!==null)
+        {
+            photo=this.state.photo;
+        }
+        return (
+
+            <View style={styles.container}>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    {photo && (
+                        <Image
+                            source={{ uri: photo.uri }}
+                            style={{ width: 300, height: 300 }}
+                        />
+                    )}
+                    <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' }} />
+                    <TextInput style={styles.inputs}
+                        placeholder="Email"
+                        keyboardType="email-address"
+                        underlineColorAndroid='transparent'
+                        onChangeText={(email) => this.setState({ email })} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' }} />
+                    <TextInput style={styles.inputs}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        underlineColorAndroid='transparent'
+                        onChangeText={(password) => this.setState({ password })} />
+                </View>
+
+                <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]}
+                    onPress={() => this.onSubmit()}>
+                    <Text style={styles.loginText}>Реєстрація</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('login')}>
+                    <Text>Login</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#DCDCDC',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 30,
+        borderBottomWidth: 1,
+        width: 250,
+        height: 45,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputs: {
+        height: 45,
+        marginLeft: 16,
+        borderBottomColor: '#FFFFFF',
+        flex: 1,
+    },
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
+    },
+    loginButton: {
+        backgroundColor: "#00b5ec",
+    },
+    loginText: {
+        color: 'white',
+    }
+});
